bf8dfcbbd3cac667a39998dce464c175a823bad5
Eduardo Goncalves <edugsdf@gmail.com>
2021-04-23 12:12:59 -0300

    Criado o Model, Factory, seeder e controller de Category

---
+++ app/Models/Category.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Traits\Uuid as TraitsUuid;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Category extends Model
+{
+    use SoftDeletes, TraitsUuid;
+    protected $fillable = ['name', 'description', 'is_active'];
+    protected $dates = ['deleted_at'];
+
+    public $incrementing = false;
+    protected $keyType = 'string';
+}
